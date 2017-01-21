c67774219329c563ac35bca20c5091796a862acd
jompie <jeanpaulweijers@gmail.com>
2017-01-21 11:07:07 +0100

    server controls demo

---
+++ RentMyAnything/RentMyAnything/Demonstrations/ServerControls.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RentMyAnything.Demonstrations
+{
+    public partial class ServerControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            displayLabel.Text = demoToolBox.Text; //set text of the label to text from text box
+            demoToolBox.Text = string.Empty; //empty the text box
+        }
+    }
+}
