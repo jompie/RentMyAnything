1a2100ea86aaf64458877df434889474317c261f
jompie <jeanpaulweijers@gmail.com>
2017-01-28 17:32:30 +0100

    Created MVC layout page

---
+++ RentMyAnything/RentMyAnything/WebFormsBaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentMyAnything
+{
+    public class WebFormsBaseClass : System.Web.UI.Page
+    {
+        public string MetaTagKeywords { get; set; }
+        public string MetatagDescription { get; set; }
+
+    }
+    }
+    }
+}
