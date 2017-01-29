00cb5b02f5620b951e38921b59f8c901299c6ed9
jompie <jeanpaulweijers@gmail.com>
2017-01-29 09:54:22 +0100

    Added styling to menu control

--- RentMyAnything/RentMyAnything/WebForms.Master.designer.cs
+++ RentMyAnything/RentMyAnything/WebForms.Master.designer.cs
@@ -31,13 +31,13 @@ namespace RentMyAnything {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Menu1 control.
+        /// LeftNavigation control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu Menu1;
+        protected global::System.Web.UI.WebControls.Menu LeftNavigation;
         
         /// <summary>
         /// SiteMapDataSource1 control.
